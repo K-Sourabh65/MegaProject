d986327f20be45bdc2c008c61c624842d2742e8d
sourabh <sourabh@tacitbase.com>
2024-05-08 13:39:19 +0530

    viewproject

--- frontend/src/components/ViewProject.js
+++ frontend/src/components/ViewProject.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from "react-router-dom";
-import '../styles/ViewProject.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import '../styles/ViewInterview.css';
 
 export default function ViewProject(props) {
   const [project, setProject] = useState({});
   const projectId = props.location.state.projectId;
 
   useEffect(() => {
-    console.log("projectId", projectId)
     fetch(`http://localhost:8000/viewproject/${projectId}`, {
       method: 'GET',
     })
@@ -19,22 +20,29 @@ export default function ViewProject(props) {
 
   return (
     <>
-      <div className='project-container'>
-        <div className='project-card'>
-          <div className='project-img'>
+      <div className='vinter-container'>
+        <div className='vinter-card'>
+          <div className='inter-img'>
             <img src={project.ProjectImage} alt="png" />
           </div>
           <div className='info'>
             <h3>{project.ProjectName}</h3>
             <div className='title'>
-              <h4>{project.ProjectDetail}</h4>
-              <h6>{project.ProjectLink}</h6>
+              {/* <h4>{project.ProjectDetail}</h4> */}
+              <h6>
+                <a href={project.ProjectLink} target="_blank" rel="noopener noreferrer">
+                  Link <FontAwesomeIcon icon={faExternalLinkAlt} />
+                </a>
+              </h6>
             </div>
           </div>
-          <div className='result'>
+          {/* <div className='result'>
             <h4>{project.ProjectStudentEmail}</h4>
-          </div>
+          </div> */}
         </div>
+      </div>
+      <div className='inter-card-1'>
+        {project.ProjectDetail}
       </div>
     </>
   );
